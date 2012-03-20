57779779ba1201c679943846780798d52044781c
Fodoj <fodojyko@gmail.com>
2012-03-21 01:53:55 +0600

    diff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,7 @@
 //= require plugins/jquery.jscrollpane.min.js
 
 $(function() {
-  $('.thumbnails-scroller').css('height', $('.post-body').height());
+  $('.thumbnails-scroller').height($('.post-body').height());
   $('.thumbnails-scroller').jScrollPane({
     autoReinitialise: true
   });
