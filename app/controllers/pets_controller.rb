5f482e94fa6357d399f7eb53589af743ee237837
Fodoj <fodojyko@gmail.com>
2012-04-06 16:04:33 +0600

    Random pet previews

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,6 +7,7 @@ class PetsController < ApplicationController
 
   def show
     @pet = Pet.find(params[:id])
+    @previews = @pet.photos.order("RANDOM()").last(4)
     @parents = @pet.parents
     @grandparents = Pet.extend_parents(@pet.parents)
     @elders = Pet.extend_parents(@grandparents)
