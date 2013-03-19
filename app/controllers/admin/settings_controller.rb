e169c450436fb2e99544e9a6c31114f820b5b755
Denis Krivostchokov <densomart@gmail.com>
2013-03-19 12:02:41 +0700

    Add more setting fields

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -3,13 +3,19 @@ class Admin::SettingsController < Admin::ApplicationController
   def show
     @site_name=AdminSettings.site_name
     @adress=AdminSettings.adress
+    @owner_name=AdminSettings.owner_name
+    @phone_number=AdminSettings.phone_number
+    @subscribe_send=AdminSettings.subscribe_send
   end
 
   def update
-    # Logger.new("#{Rails.root}/log/debugger.log").info "#{Time.now.strftime("at %R")} in SettingsController - update || #{params}"
+    Logger.new("#{Rails.root}/log/debdeb.log").info "#{Time.now.strftime("at %R")} in SettingsController - update || #{params}"
 
     AdminSettings.site_name=params[:site_name]
     AdminSettings.adress=params[:adress]
+    AdminSettings.owner_name=params[:owner_name]
+    AdminSettings.phone_number=params[:phone_number]
+    params[:subscribe_send] ? AdminSettings.subscribe_send=true : AdminSettings.subscribe_send=false
     
     if true #todo validate
       redirect_to(:admin_settings,
