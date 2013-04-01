d567f87b45f405b72035c16be32c70695220d8a2
Denis Krivostchokov <densomart@gmail.com>
2013-04-01 19:13:04 +0700

    erase the SiteSettings class files from the project

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Kennel::Application.initialize!
 
-#Initialize settings store
-SiteSettings::STORE_TYPE=:Yaml # select :Yaml or ActiveRecord::Store
-AdminSettings=SiteSettings::Settings.instance
--- db/migrate/20130327102241_create_site_settings.rb
+++
@@ -1,7 +0,0 @@
-class CreateSiteSettings < ActiveRecord::Migration
-  def change
-    create_table :site_settings do |t|
-      t.text :settings
-    end
-  end
-end
